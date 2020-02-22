926f4a6bcf4616979d2de08eb8aa4a8b0ec754fb
HADMARINE <tsx@kakao.com>
2020-02-23 01:32:52 +0900

    Update Wrapper.tsx
    Type update

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,9 +1,11 @@
-import styled, { ThemedStyledFunction } from 'styled-components';
+import styled, { StyledComponent } from 'styled-components';
+
+type StyledComponentDiv = StyledComponent<'p', any, Record<string, any>>;
 
 interface FlexTypes {
-  center: ThemedStyledFunction<'div', any> & any;
-  left: ThemedStyledFunction<'div', any> & any;
-  right: ThemedStyledFunction<'div', any> & any;
+  center: StyledComponentDiv;
+  left: StyledComponentDiv;
+  right: StyledComponentDiv;
 }
 
 export const Flex: FlexTypes = {
