62d22ec4dd7b65f912e29b702a8fb071cd91ce8e
HADMARINE <tsx@kakao.com>
2020-02-23 01:32:45 +0900

    Update Text.tsx
    Type update
    CSS Code change (word-break)

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,14 +1,25 @@
-import styled from 'styled-components';
+import styled, { StyledComponent } from 'styled-components';
 
-export const Title: any = styled.p`
+// Interface & types
+type ParagraphType = StyledComponent<'p', any, Record<string, any>>;
+
+interface BoldType {
+  color: StyledComponent<'span', any, Record<string, any>>;
+  default: StyledComponent<'span', any, Record<string, any>>;
+}
+
+// Styles + Components
+
+export const Title: StyledComponent<'p', any, Record<string, any>> = styled.p`
   font-size: 50px;
   font-weight: ${(props: any): number =>
     props.fontWeight ? props.fontWeight : 900};
+  word-break: keep-all;
 `;
 
-export const SubTitle: any = styled.p``;
+export const SubTitle: ParagraphType = styled.p``;
 
-export const Bold: any = {
+export const Bold: BoldType = {
   color: styled.span`
     color: #ea5550;
     font-weight: 900;
@@ -18,10 +29,11 @@ export const Bold: any = {
   `,
 };
 
-export const Paragraph: any = styled.p`
+export const Paragraph = styled.p`
   font-size: 20px;
   font-weight: 300;
   line-height: 1.8;
+  word-break: keep-all;
 `;
 
 const Text = { Title, Paragraph, Bold };
